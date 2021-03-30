022f57b8ad802e15760c4ff32dcd5f70b9c86bcc
AlexPro88 <shoptechnikdirekt@gmail.com>
2021-03-31 02:05:07 +0300

    add Products of week

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,11 @@ $(function() {
         autoplay: true,
         autoplaySpeed: 2000
     });
+
+
+    // $('.menu-filter__item').on('click', function() {
+    //     $('.menu-filter__item').removeClass('menu-filter__item--active');
+    //     $('this').toggleClass('menu-filter__item--active');
+
+    // });
 });
